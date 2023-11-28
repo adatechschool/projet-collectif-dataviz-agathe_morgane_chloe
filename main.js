8b511c9a484b88141400a31df6865b19dfe40350
Chloé T. <chloe.thomas14@gmail.com>
2023-11-28 16:36:08 +0100

    grosses modif

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // select marques
-const brands = ["Choisir une marque", "Ethiquable", "Milka", "Kaoka", "Kinder", "Bonnat", "Lindt", "Michel Cluizel"]
-const categories = ["Choisir une catégorie", "Snacks", "Chocolats", "Chocolats noirs", "Bars", "Pâtes à tartiner"]
-
+const brands = ["Choisir une marque", "Ethiquable", "Milka", "Kaoka", "Kinder", "Bonnat", "Lindt", "Michel-Cluizel"]
+const categories = ["Choisir une catégorie", "Snacks", "Chocolats", "Chocolats noirs", "Barres", "Pâtes à tartiner", "Chocolats de Noël"]
+const catTrad = ["Choisir une catégorie", "snacks", "chocolates", "dark-chocolates", "bars", "chocolate-spreads", "christmas-chocolates"]
 
 const selectBrands = () => {
 	const selectBrand = document.getElementById("brand-select");
@@ -23,7 +23,6 @@ const selectCategories = () => {
 		selectCat.add(option)
 	}
 }
-
 selectBrands()
 selectCategories()
 
@@ -32,27 +31,62 @@ const getValueBrand = () => {
 	const selectBrandId = document.getElementById("brand-select");
 	const brandChoice = selectBrandId.selectedIndex;
 	const brandResult = selectBrandId.options[brandChoice].value;
-	console.log("brand:", brandResult)
 	return brandResult
 }
 const getValueCat = () => {
 	const selectCatId = document.getElementById("cat-select");
 	const catChoice = selectCatId.selectedIndex;
 	const catResult = selectCatId.options[catChoice].value;
-	console.log("category:", catResult)
-	return catResult
+
+	//Traduction de la catégorie. Va chercher l'index du tableau categories et va ensuite chercher la traduction dans le tableau catTrad
+	const valueCatIndex = categories.indexOf(catResult)
+	const catResultTrad = catTrad[valueCatIndex]
+	return catResultTrad
 }
 
+const affichage = (response) => {
+const listFiches = document.querySelector(".listFiches")
+
+	for (let i=0; i<response.length; i++) {
+		const article = response[i]
+
+		const fiche = document.createElement("div")
+		
+
+		const productName = document.createElement("h2")
+		productName.textContent = article.product_name_fr
+
+
+		const brand = document.createElement("h3")
+		brand.textContent = article.brands
+
+		const nutriscoreTag = document.createElement("p")
+		nutriscoreTag.textContent = article.nutriscore_grade
+
+		const nutriscoreViz = document.createElement("img")
+		const nutriscore = article.nutriscore_grade
+		nutriscoreViz.src = `img/nutri_${nutriscore}.png`
+
+	}
+
+
+}
 
 const request = async () => {
-	const valueBrand=getValueBrand();
-	const valueCat=getValueCat();
-	console.log("appel:",valueBrand, valueCat);
-	const url = `https://openfoodfacts.github.io/openfoodfacts-server/api/ref-v2/#get-/api/v2/search?brands_tag=${valueBrand}&categories_tags=${valueCat}&sort_by=nutriscore_score`;
-	console.log(url);
+	const valueBrand = getValueBrand();
+	const valueCat = getValueCat();
+
+	const url = `https://world.openfoodfacts.net/api/v2/search?brands_tags=${valueBrand}&categories_tags=${valueCat}&countries_tags_en=france&fields=brands%2Cproduct_name_fr%2Cnutriscore_grade&sort_by=nutriscore_score`;
 	const datas = await fetch(url);
 	const response = await datas.json();
-	console.log(response)
+	console.log("méga objet = ", response)
+	console.log("response.product =", response.products)
+
+	//Limiter à 5 réponses
+	const responseFive = response.products.slice(0, 5)
+	console.log("5 requêtes = ", responseFive)
+
+	affichage(responseFive)
+
 }
-//to do : limiter la requête à 5 meilleures réponses
 // to do : gérer une image par défaut grâce à ??
