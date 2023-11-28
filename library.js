0c00a861165ca95e65a4ec1f71468378480122fc
Chloé T. <chloe.thomas14@gmail.com>
2023-11-28 11:18:44 +0100

    modifications

---
+++ library.js
@@ -0,0 +1,9 @@
+let typed = new Typed('.auto-typing', {
+    strings: ['la magie de noël','healthy','du chocolat'],
+    typeSpeed:100,
+    backSpeed:100,
+    loop: true,
+    fadeOut: true,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay:500
+})
